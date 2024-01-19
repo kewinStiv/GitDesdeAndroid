1106a8fd5ff965ab3dbb3356c478c7e65fec35a9
Kewin <kewinstiv23gmail.com>
2024-01-19 18:04:05 -0500

    include prin uno

--- app/src/main/java/com/example/gitapp/MainActivity.kt
+++ app/src/main/java/com/example/gitapp/MainActivity.kt
@@ -8,9 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         print("Este es un ejemplo")
-        print("Este es un ejemplo")
-        print("Este es un ejemplo")
-        print("Este es un ejemplo")
-        print("Este es un ejemplo")
+
     }
 }
